ac41f447bf010ec24638801be76bc7a6d61f802d
thiagosnx <thiagosantanacq@gmail.com>
2025-02-01 18:43:52 -0300

    feat: post

--- app/Http/Controllers/Api/CursoController.php
+++ app/Http/Controllers/Api/CursoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Curso;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -29,21 +30,21 @@ class CursoController extends Controller
         return response()->json($cursos);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    
     public function store(Request $request)
     {
         $dados = $request->all();
         $curso = new Curso;
         $curso->ttl = $dados['titulo'];
-        $curso->rls_dt = $dados['data_lancamento'];
+        $curso->dt_lcmt = $dados['data_lancamento'];
         $curso->cghr = $dados['carga_horaria'];
         $curso->img = $dados['img'];
         $curso->url = $dados['url'];
-        $curso->ntc = $dados['noticia'];
+        $curso->tip = $dados['tipo'] || 0;
+        $curso->ntc = $dados['noticia'] || 0;
         $curso->emp = $dados['empresa'];
         $curso->save();
+        return response()->json($curso, 201);
 
     }
 
